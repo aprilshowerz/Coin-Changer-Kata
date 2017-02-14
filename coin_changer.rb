4066b5d705a1a2909595608532fffb5fc00139f6
April Blair <a.m.blair@eagle.clarion.edu>
2017-02-14 15:34:11 -0500

    6 good tests

--- coin_changer.rb
+++ coin_changer.rb
@@ -1,3 +1,70 @@
-def coin_changer(number)
-	1
+
+# Coin changer instructions:
+
+# Write a function that takes a number of cents, and that returns a hash containing the least number of coins needed to generate the value of cents.
+
+# eg. If you have 11 cents you should return {:dime => 1, :penny => 1};
+# eg. If you have 31 cents you should return {:quarter => 1, :nickel => 1, :penny => 1};
+# eg. If you have 93 cents {:quarter => 3, :dime => 1, nickel => 1, :penny => 3}
+
+def coin_changer(cents)
+	wallet = {}
+
+	dime_value = 10
+	quarter_value = 25
+	#remainder = 0
+
+	#available_coins  = {"Quarter" => 25, "Dime" => 10, "Nickel" => 5, "Penny" => 1}
+	
+	if cents == 0
+		return wallet
+	end
+
+	if cents >= 25
+		wallet[:quarter] = cents/quarter_value
+		cents = cents - 25
+	end
+
+	if cents >= 20
+		wallet[:dime] = cents/dime_value
+		cents = cents - 20
+	end
+
+	if cents >= 10
+		wallet[:dime] = 1
+		cents = cents - 10
+	end
+
+	if cents >= 5
+		wallet[:nickel] = 1
+		cents = cents - 5
+	end
+
+	if cents >= 1
+		wallet[:penny] = cents
+	end
+	wallet
+
 end
+
+
+  # def change(amount)
+
+  # 	# hash to hold the list of available coins.
+  #   available_coins  = {"Quarter" => 25, "Dime" => 10, "Nickel" => 5, "Penny" => 1}
+  #   # holds list of coins to return
+  #   coins            = {}     
+  #   # holds the remaining amount of money to change.    
+  #   remaining_amount = amount
+
+  #   # counts down finds biggest coins first (iterates)
+  #   available_coins.each do |coin| 
+  #   	if ((remaining_amount/coin).to_int > 0)
+  #   		then (remaining_amount/coin).to_int.times { coins << coin }
+  #       	puts "Remaining Amount: #{remaining_amount} | Coin: #{coin}"
+  #       	remaining_amount = amount - coins.inject(:+)
+  #   		end                         # coins.inject(:+) sums hash items
+  #   	end                           
+  #   puts "Amount #{amount} >> Coins: #{coins}\n\n"
+  #   return coins
+  # end
--- test_coin_changer.rb
+++ test_coin_changer.rb
@@ -3,10 +3,47 @@ require_relative 'coin_changer.rb'
 
 class Test_coin_changer < Minitest::Test
 
-	def test_one_returns_one
-		assert_equal(1, coin_changer(1))
+	def test_zero_cents_returns_empty_hash
+		assert_equal({}, coin_changer(0))
 	end
 
+	def test_one_cent_returns_one_penny
+		assert_equal({:penny => 1}, coin_changer(1))
+	end
+	
+	def test_2_cents_returns_2_pennies
+		assert_equal({:penny => 2}, coin_changer(2))
+	end
 
+	def test_5_cents_returns_1_nickel
+		assert_equal({:nickel => 1}, coin_changer(5))
+	end
 
+	def test_6_cents_returns_1_penny_and_1_nickel
+		assert_equal({:penny => 1, :nickel => 1}, coin_changer(6))
+	end
+
+	def test_10_cents_returns_1_dime
+		assert_equal({:dime => 1}, coin_changer(10))
+	end
+
+	def test_2_cents_returns_2_pennies
+		assert_equal({:penny => 2}, coin_changer(1))
+	end
+
+	def test_2_cents_returns_2_pennies
+		assert_equal({:penny => 2}, coin_changer(1))
+	end
+
+	def test_2_cents_returns_2_pennies
+		assert_equal({:penny => 2}, coin_changer(1))
+	end
+
+	def test_2_cents_returns_2_pennies
+		assert_equal({:penny => 2}, coin_changer(1))
+	end
+
+	def test_2_cents_returns_2_pennies
+		assert_equal({:penny => 2}, coin_changer(1))
+	end
 end
